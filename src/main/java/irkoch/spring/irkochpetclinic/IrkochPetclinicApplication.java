d11cb97f4e8a0d1a360266971de051030b88535d
Abdessamad CHOKRI <abdessamad@stschain.com>
2019-12-05 11:49:50 +0100

    initial commit add

---
+++ src/main/java/irkoch/spring/irkochpetclinic/IrkochPetclinicApplication.java
@@ -0,0 +1,13 @@
+package irkoch.spring.irkochpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrkochPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrkochPetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/irkoch/spring/irkochpetclinic/IrkochPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package irkoch.spring.irkochpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrkochPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
